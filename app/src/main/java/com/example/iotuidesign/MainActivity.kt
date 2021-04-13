4837e3391ce1b68e63b3e11f19ee5560da1a02ad
terrylim1111 <terrylim1111@gmail.com>
2021-04-14 02:11:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/iotuidesign/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.iotuidesign
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.os.HandlerCompat.postDelayed
+
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val intent = Intent( this@MainActivity, Homepage::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
