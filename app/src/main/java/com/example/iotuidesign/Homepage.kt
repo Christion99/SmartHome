f2e48097408d9721dca7e972311c79524fc07322
terrylim1111 <terrylim1111@gmail.com>
2021-04-14 20:06:25 +0800

    ultrasonic

--- app/src/main/java/com/example/iotuidesign/Homepage.kt
+++ app/src/main/java/com/example/iotuidesign/Homepage.kt
@@ -1,11 +1,18 @@
 package com.example.iotuidesign
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_homepage.*
 
 class Homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+
+        btnTank.setOnClickListener{
+            val intent = Intent (this, Watertank::class.java)
+            startActivity(intent)
+        }
     }
 }
