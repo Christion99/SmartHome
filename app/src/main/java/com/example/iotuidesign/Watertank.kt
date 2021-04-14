c2db3a72ab6fdf180f21017172281f678ffc6d41
terrylim1111 <terrylim1111@gmail.com>
2021-04-15 02:07:52 +0800

    ultrasonic

--- app/src/main/java/com/example/iotuidesign/Watertank.kt
+++ app/src/main/java/com/example/iotuidesign/Watertank.kt
@@ -1,100 +1,6 @@
-/*
 package com.example.iotuidesign
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
-import android.widget.ProgressBar
-import androidx.appcompat.app.ActionBar
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import kotlinx.android.synthetic.main.activity_watertank.*
-
-class Watertank : AppCompatActivity(){
-
-    private var database = FirebaseDatabase.getInstance()
-    private lateinit var sensorData:String
-    private lateinit var sensorUltra: ValueEventListener
-    var myRefs = database.getReference("PI_007").child("Ultrasonic")
-    //private val path = "UltraSonic"
-
-    private var progressBar: ProgressBar? = null
-
-    val tag = "DebuggingIOT"
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_watertank)
-        val actionBar: ActionBar = supportActionBar!!
-        actionBar.title = "Water Tank Level"
-        actionBar.setDisplayHomeAsUpEnabled(true)
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            getUltraDate()
-        }, 2000)
-
-        //getUltraDate()
-    }
-
-    private fun getUltraDate() {
-        progressBar = findViewById<ProgressBar>(R.id.indicator)
-
-        sensorUltra = object : ValueEventListener {
-            override fun onCancelled(p0: DatabaseError) {
-                Log.d(tag, "${p0.toException()}")
-            }
-
-            override fun onDataChange(p0: DataSnapshot) {
-                val data = p0.value
-                water_value.text = data.toString()
-
-                //progressBar!!.progress = data.toString().toInt() / 5
-                //percentage.text = (data.toString().toInt()/ 5).toString()
-
-                /*
-                if (p0.exists()) {
-                    val child = p0.children
-                    for (data in child) {
-                        sensorData =
-                            data.child("Ultrasonic").value.toString()//change the path variable
-                        water_value.text = (sensorData.toDouble()).toString()
-                        progressBar!!.progress = sensorData.toInt() / 5
-                        percentage.text = (sensorData.toInt() / 5).toString()
-                    }
-                }
-                val test = p0.value
-                //Log.d("Test", test.toString())
-                //myRefs.addValueEventListener(sensorUltra)
-                */
-            }
-        }
-    }
-
-    private fun destroyListeners(){
-        myRefs.removeEventListener(sensorUltra)
-    }
-
-    override fun onDestroy() {
-        Log.d(tag, "UltrasonicOnDestroy")
-        destroyListeners()
-        super.onDestroy()
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        this.finish()
-        return true
-    }
-
-}
-*/
-
-package com.example.iotuidesign
-
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -124,12 +30,14 @@ class Watertank : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_watertank)
         val actionBar: ActionBar = supportActionBar!!
-        actionBar.title = "Water Tank Level"
+        actionBar.title = "Water Tank"
         actionBar.setDisplayHomeAsUpEnabled(true)
 
         Handler(Looper.getMainLooper()).postDelayed({
             getUltraDate()
         }, 2000)
+
+
     }
 
     private fun getUltraDate() {
@@ -140,12 +48,24 @@ class Watertank : AppCompatActivity() {
                 Log.d(tag, "${p0.toException()}")
             }
 
+            @SuppressLint("SetTextI18n")
             override fun onDataChange(p0: DataSnapshot) {
                 val data = p0.value
-                //sensorData = data.child("UltraSonic").value.toString()//change the path variable
                 water_value.text = data.toString()
+                //setStatus(water_value.toString())
                 progressBar!!.progress = data.toString().toInt() / 5
                 percentage.text = (data.toString().toInt() / 5).toString()
+                val status = percentage.toString()
+                if (status < 100.toString()) {
+                    txtDetail.text = "There's a problem with the water tank. Water level is below 100 Litre"
+                } else if (status <= 475.toString()) {
+                    txtDetail.text = "There's a problem with the water tank. Water level is over 475 Litre"
+                } else if (status >= 100.toString() && status<=475.toString()) {
+                    txtDetail.text = "Water tank status is good."
+                }
+                /*else{
+                    txtDetail.text = "Error! Please manually check the water tank."
+                }*/
             }
         }
         myRefs.addValueEventListener(sensorUltra)
@@ -166,5 +86,21 @@ class Watertank : AppCompatActivity() {
         this.finish()
         return true
     }
+
+/*
+    @SuppressLint("SetTextI18n")
+    private fun setStatus(status: String) {
+        if (status < 100.toString()) {
+            txtDetail.text = "There's a problem with the water tank. Water level is below 100 Litre"
+        } else if (status <= 475.toString()) {
+            txtDetail.text = "There's a problem with the water tank. Water level is over 475 Litre"
+        } else if (status >= 100.toString() && status<=475.toString()) {
+            txtDetail.text = "Water tank status is good."
+        }else{
+            txtDetail.text = "Error! Please manually check the water tank."
+        }
+        txtDetail. = SystemClock.elapsedRealtime() + 20000
+    }
+*/
 
 }
